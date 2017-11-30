567bd7fcfa9b4ab75f61b8c61975ad7bca278990
Sri Harsha <sriharrsha@users.noreply.github.com>
2017-11-30 02:15:01 -0600

    Update users.py

--- src/C3STEM/DB/users.py
+++ src/C3STEM/DB/users.py
@@ -9,7 +9,7 @@ sys.path.insert(0, '/opt/C3STEM/Middleware')
 connection = MongoClient()
 db = connection.c3stem_database
 
-with open('/opt/C3STEM/Middleware/users.csv', 'rb') as f:
+with open('/opt/C3STEM/DB/users.csv', 'rb') as f:
     reader = csv.reader(f)
     for row in reader:
     	hashed_pass = bcrypt.hashpw(row[1].encode('utf-8'), bcrypt.gensalt())
