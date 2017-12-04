7a3794025a9343c70f7efb510ff996320af9e103
Zhuangwei Kang <ZhuangweiKang@users.noreply.github.com>
2017-12-04 14:12:30 -0600

    Update c2sumo_schema_nashville.py

--- src/C3STEM/DB/c2sumo_schema_nashville.py
+++ src/C3STEM/DB/c2sumo_schema_nashville.py
@@ -12,8 +12,7 @@ from junction1_data_nashville import *
 from junction2_data_nashville import *
 from default_problem_data import *
 
-MONGO_HOST = "129.59.107.35"
-MONGO_PORT = 27017
+from Mongodb import *
 
 connection = MongoClient(MONGO_HOST, MONGO_PORT)
 connection.drop_database('c3stem_database')
